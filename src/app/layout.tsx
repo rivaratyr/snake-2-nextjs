d53c0d390566afc30ae163a096f1d249ee11bc7b
Rivara Tyr <rivara.tyr@gmail.com>
2025-06-01 00:26:21 +0200

    Refactored root layout to use World login + Next auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,27 @@
 // app/layout.tsx
 import './globals.css';
 import { ReactNode } from 'react';
-import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider';
+import { auth } from '@/auth';
+import ClientProviders from '@/providers';
+import '@worldcoin/mini-apps-ui-kit-react/styles.css';
+import type { Metadata } from 'next';
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export const metadata: Metadata = {
+  title: 'Snake2',
+  description: 'The Ultimate PVP Snake',
+};
 
+export default async function RootLayout({ children }: { children: ReactNode }) {
+
+  const session = await auth();
+    
   return (
+    
     <html lang="en">
       <body className="bg-gray-100 text-gray-900">
-        <MiniKitProvider>
+        <ClientProviders session={session}>
         {children}
-        </MiniKitProvider>
+        </ClientProviders>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 // app/page.tsx
 import { redirect } from 'next/navigation';
+import { Page } from '@/components/PageLayout';
+import { AuthButton } from '../components/AuthButton';
 
 export default function Home() {
-  redirect('/lobby');
+  //redirect('/lobby');
+  return (
+    <Page>
+      <Page.Main className="flex flex-col items-center justify-center">
+        <AuthButton />
+      </Page.Main>
+    </Page>
+  );
 }
